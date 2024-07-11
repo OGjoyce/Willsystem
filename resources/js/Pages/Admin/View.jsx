b67d2690a6d10aa6f5ee0aa8cc85ee838c010d43
Carlos <montiel@galileo.edu>
2024-07-10 20:56:21 -0600

    added view for pdf

--- resources/js/Pages/Admin/View.jsx
+++ resources/js/Pages/Admin/View.jsx
@@ -12,15 +12,49 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Link, Head, router } from '@inertiajs/react';
 import axios from 'axios';
 
-
+import PDFEditor from '@/Components/PDF/PDFEditor';
 import { Form, Button, Container, Row, Col, Table } from 'react-bootstrap';
+import WillContent from '@/Components/PDF/Content/WillContent'
+import POA1Content from '@/Components/PDF/Content/POA1Content';
+import POA2Content from '@/Components/PDF/Content/POA2Content';
+
+import Modal from 'react-bootstrap/Modal';
 
 var ArrObj = [];
+let selectedData;
+let allDataFetched;
+let currIdSelected = 0
+
 const View = () => {
     const [objStatuses, setObjStatuses] = useState([]);
     const [packageValue, setPackageValue] = useState('');
     const [results, setResults] = useState([]);
+    const [show, setShow] = useState(false);
+    const [docSelected, setDocSelected] = useState("");
+    const [idSelected, setIdSelected] = useState("");
+    const handleClose = () => {
 
+        setShow(false);
+    
+    }
+    function searchById(file){
+        debugger;
+        console.log(file);
+        let results = allDataFetched.filter(item => item.id === idSelected);
+        results = results[0].information;
+        console.log(results);
+        setDocSelected(file);
+        console.log(idSelected);
+        console.log(currIdSelected);
+    
+    }
+
+    const handleShow = (id) => {
+        setIdSelected(id);
+        currIdSelected = id;
+        setShow(true);
+    }
+    
     const showDoms = (id) => {
         return true;
     }
@@ -29,10 +63,10 @@ const View = () => {
             params: { owner: packageValue }
         });
         console.log(response.data);
+        allDataFetched = response.data;
         const length = response.data.length;
         for (const element of response.data) {
-            debugger;
-            let backendData =element;
+            let backendData = element;
             let createdat = backendData.created_at;
             let updatedat = backendData.updated_at;
             let owner = backendData.information[0].owner;
@@ -74,73 +108,104 @@ const View = () => {
             <div className="py-12" style={{ height: "100%", overflow: "hidden" }}>
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8" style={{ height: "inherit" }}>
                     <div className="bg-white overflow-visible shadow-sm sm:rounded-lg container" style={{ height: "inherit" }}>
-                        <Container style={{ padding: "10px" }}>
-                            <Form>
-                                <Row>
-                                    <Col sm={8}>
-                                        <Form.Group className="mb-3" controlId="emailid">
-                                            <Form.Control onChange={(e) => setPackageValue(e.target.value)} type="email" placeholder="the owner of the will..." />
-                                        </Form.Group>
-
-
-                                    </Col>
-                                    <Col sm={4}>
-                                        <Button onClick={handleSearch} type='button' className='outline-dark'>Search</Button>
-
-                                    </Col>
-                                </Row>
-                            </Form>
-                            <br></br>
-                            <br></br>
-                            <p>Please, search by email of the owner of the will, then search it on the table</p>
-                            <Table striped bordered hover responsive>
-                                <thead>
-                                    <tr>
-                                        <th>File id</th>
-                                        <th>Email</th>
-                                        <th>Name</th>
-                                        <th>Created</th>
-                                        <th>Last Modification</th>
-                                        <th>Watch Files</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        ArrObj.map((item, index) => (
-                                            <tr key={index}>
-                                                <td>{item.id}</td>
-                                                <td>{item.email}</td>
-                                                <td>{item.name}</td>
-                                                <td>{item.created}</td>
-                                                <td>{item.updated}</td>
-                                                <td>
-                                                    <Button variant="outline-warning" size="sm" onClick={() => showDoms(item.id)}><i class="bi bi-eye"></i>View Documents</Button>
-                                                </td>
-                                            </tr>
-                                        ))
+                        {
+                            docSelected != "" ?
+                                <PDFEditor
+                                    ContentComponent={
+                                        docSelected === 'Will' ? WillContent :
+                                            docSelected === 'POA1' ? POA1Content :
+                                                POA2Content
                                     }
+                                    datas={results}
+                                />
+                                :
+                                <><Container style={{ padding: "10px" }}>
+                                    <Form>
+                                        <Row>
+                                            <Col sm={8}>
+                                                <Form.Group className="mb-3" controlId="emailid">
+                                                    <Form.Control onChange={(e) => setPackageValue(e.target.value)} type="email" placeholder="the owner of the will..." />
+                                                </Form.Group>
+
+
+                                            </Col>
+                                            <Col sm={4}>
+                                                <Button onClick={handleSearch} type='button' className='outline-dark'>Search</Button>
+
+                                            </Col>
+                                        </Row>
+                                    </Form>
+                                    <br></br>
+                                    <br></br>
+                                    <p>Please, search by email of the owner of the will, then search it on the table</p>
+                                    <Table striped bordered hover responsive>
+                                        <thead>
+                                            <tr>
+                                                <th>File id</th>
+                                                <th>Email</th>
+                                                <th>Name</th>
+                                                <th>Created</th>
+                                                <th>Last Modification</th>
+                                                <th>Watch Files</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {ArrObj.map((item, index) => (
+                                                <tr key={index}>
+                                                    <td>{item.id}</td>
+                                                    <td>{item.email}</td>
+                                                    <td>{item.name}</td>
+                                                    <td>{item.created}</td>
+                                                    <td>{item.updated}</td>
+                                                    <td>
+                                                        <Button variant="outline-warning" size="sm" onClick={() => handleShow(item.id)}><i class="bi bi-eye"></i>View Documents</Button>
+                                                    </td>
+                                                </tr>
+                                            ))}
 
 
 
 
-                                </tbody>
-                            </Table>
-                        </Container>
+                                        </tbody>
+                                    </Table>
+                                </Container><Modal show={show} onHide={handleClose}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Select the document you want to see then click Go</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <Row className="mt-3">
+                                                <Col>
+                                                    <Button onClick={() => searchById('Will', show)} style={{ width: "100%" }} variant="outline-dark"> <i class="bi bi-file-text"></i> Will</Button>
+                                                </Col>
+                                                <Col>
+                                                    <Button onClick={() => searchById('POA1', show)} style={{ width: "100%" }} variant="outline-dark"> <i class="bi bi-house"></i> POA1 Property</Button>
+                                                </Col>
+                                                <Col>
+                                                    <Button onClick={() => searchById('POA2', show)} style={{ width: "100%" }} variant="outline-dark"> <i class="bi bi-hospital"></i> POA2 Health</Button>
+                                                </Col>
+                                            </Row>
 
-                        <div>
-                            {/* <input
-                                type="text"
-                                value={packageValue}
-                                onChange={(e) => setPackageValue(e.target.value)}
-                                placeholder="Search by package" />
-                            <button onClick={handleSearch}>Search</button> */}
-                            <ul>
-                                {/* {results.map(file => (
-                                    <li key={file.id}>{file.details.packages}</li>
-                                ))} */}
-                            </ul>
-                        </div>
+
+
+                                        </Modal.Body>
+                                        <Modal.Footer>
+                                            <Button variant="secondary" onClick={handleClose}>
+                                                Close
+                                            </Button>
+                                            <Button variant="primary" onClick={handleClose}>
+                                                Go
+                                            </Button>
+                                        </Modal.Footer>
+                                    </Modal></>
+
+
+                        }
+
+
+
                     </div></div></div>
+
+
 
 
         </AuthenticatedLayout></>
