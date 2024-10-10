3e95e20319d16c28f00c5462927417274f7046e9
abrahaamv <velizabrahaam@gmail.com>
2024-10-10 16:47:50 -0600

    Update new_object_status.js

--- resources/js/__test__/new_object_status.js
+++ resources/js/__test__/new_object_status.js
@@ -1,23 +1,30 @@
-const [objectStatu, setObjectStatus] = useState({
+const [objectStatus, setObjectStatus] = useState({
   owner: null,
   packageInfo: null,
-  profiles: {
-    self: {},
-    spouse: {},
-    children: [],
-    others: {}, // For secondary wills or additional profiles
+  personInfo: {
+    primary: {
+      self: {},
+      spouse: {},
+      children: [],
+    },
+    secondary: {
+      1: {
+        self: {},
+        spouse: {},
+        children: [],
+      }
+    }, // For secondary wills or additional profiles
   },
   documents: {
     primaryWill: null,
     spousalWill: null,
     secondaryWills: [], // Array to support multiple secondary wills
-    poaProperty: [], // Array to support multiple poaProperty
-    poaHealth: [], // Array to support multiple poaHealth
+    poaProperty: [], // Array to support multiple POA properties
+    poaHealth: [], // Array to support multiple POA health documents
   },
 });
 
-
-const objectStatus = {
+const object_status = {
   owner: "user@example.com",
   packageInfo: {
     "id": 2,
@@ -26,10 +33,11 @@ const objectStatus = {
     "campaign": "Facebook",
     "created_at": "2024-08-01T00:54:33.000000Z",
     "updated_at": "2024-08-01T00:55:35.000000Z",
-    "description": "2 POAS and Will"
+    "description": "2 POAs and Will"
   },
-  profiles: {
-    self: {
+  familyProfile: {
+    primary: {
+      self: {
         "city": "Toronto",
         "step": 0,
         "email": "janedoe@email.com",
@@ -38,113 +46,117 @@ const objectStatus = {
         "province": "Ontario",
         "telephone": "+1 (111) 111-1111",
         "timestamp": 1725655298320
-    },
-    spouse: {
-         "city": "Toronto",
-          "email": "jeny@email.com",
-          "phone": "+1 (111) 111-1111",
+      },
+      spouse: {
+        "city": "Toronto",
+        "email": "jeny@email.com",
+        "phone": "+1 (111) 111-1111",
+        "country": "Canada",
+        "lastName": "Doe",
+        "province": "Ontario",
+        "relative": "Spouse",
+        "firstName": "Jeny",
+        "timestamp": 1725655352589,
+        "middleName": ""
+      },
+      children: [
+        {
+          "id": 1,
+          "city": "Toronto",
           "country": "Canada",
           "lastName": "Doe",
           "province": "Ontario",
-          "relative": "Spouse",
-          "firstName": "Jeny",
-          "timestamp": 1725655352589,
-          "middleName": ""
+          "relative": "Child",
+          "firstName": "Junior"
+        },
+        {
+          "id": 2,
+          "city": "Toronto",
+          "country": "Canada",
+          "lastName": "Doe",
+          "province": "Ontario",
+          "relative": "Child",
+          "firstName": "Helen"
+        }
+      ],
+      relatives: [],
     },
-    children: [
-         {
-            "id": 1,
-            "city": "Toronto",
-            "country": "Canada",
-            "lastName": "Doe",
-            "province": "Ontario",
-            "relative": "Child",
-            "firstName": "Junior"
-          },
-          {
-            "id": 2,
-            "city": "Toronto",
-            "country": "Canada",
-            "lastName": "Doe",
-            "province": "Ontario",
-            "relative": "Child",
-            "firstName": "Helen"
-          }
-    ],
-    relatives: [
-
-    ],
-    others: {
+    secondary: {
       1: {
-        self: { /* Datos del usuario */ },
-        spouse: { /* Datos del cónyuge */ },
-        children: [ /* Datos de los hijos */ ],
+        self: { /* User data */ },
+        spouse: { /* Spouse data */ },
+        children: [ /* Children data */ ],
         relatives: []
       },
       2: {
-        self: { /* Datos del usuario */ },
-        spouse: { /* Datos del cónyuge */ },
-        children: [ /* Datos de los hijos */ ],
+        self: { /* User data */ },
+        spouse: { /* Spouse data */ },
+        children: [ /* Children data */ ],
         relatives: []
       },
-      // ...
+      // Additional profiles can be added here
     },
   },
   documents: {
     primaryWill: {
-        profileKey: 'self',
-        data: {'executors': {},'bequest': {}, "etc":{} },
-        documentDOM: { v1: {}, v2:{}},
+      profileKey: 'self',
+      data: { 'executors': {}, 'bequest': {}, "etc": {} },
+      documentDOM: { v1: {}, v2: {} },
     },
     spousalWill: {
-        profileKey: 'spouse',
-        data: {'executors': {},'bequest': {}, "etc":{} },
-        documentDOM: { v1: {}, v2:{}},
+      profileKey: 'spouse',
+      data: { 'executors': {}, 'bequest': {}, "etc": {} },
+      documentDOM: { v1: {}, v2: {} },
     },
     secondaryWills: [
       {
-        profileKey: '1', // Referencia a profiles.others[1]
-        data: {'executors': {},'bequest': {}, "etc":{} },
-        documentDOM: { v1: {}, v2:{}},
+        profileKey: '1', // Reference to secondary profile 1
+        data: { 'executors': {}, 'bequest': {}, "etc": {} },
+        documentDOM: { v1: {}, v2: {} },
       },
       {
-        profileKey: '2', // Referencia a profiles.others[2]
-        data: {'executors': {},'bequest': {}, "etc":{} },
-        documentDOM: { v1: {}, v2:{}},
+        profileKey: '2', // Reference to secondary profile 2
+        data: { 'executors': {}, 'bequest': {}, "etc": {} },
+        documentDOM: { v1: {}, v2: {} },
       },
-      // Añadir más secondaryWills según sea necesario
+      // Additional secondary wills can be added as needed
     ],
     poaProperty: [
       {
-        profileKey: 'self', // Referencia a profiles.self
-        data: { },
-        documentDOM: { v1: {}, v2:{}},
+        profileKey: 'self', // Reference to self profile
+        data: {},
+        documentDOM: { v1: {}, v2: {} },
       },
       {
-        profileKey: 'spouse', // Referencia a profiles.spouse
-        data: { /* Datos del POA Property para spouse */ },
-        documentDOM: { v1: {}, v2:{}},
+        profileKey: 'spouse', // Reference to spouse profile
+        data: { /* POA Property data for spouse */ },
+        documentDOM: { v1: {}, v2: {} },
       },
-      // Añadir más POA Properties según sea necesario
+      {
+        profileKey: '1', // Reference to secondary profile 1
+        data: { /* POA Property data for secondary profile 1 */ },
+        documentDOM: { v1: {}, v2: {} },
+      },
+      // Additional POA Properties can be added as needed
     ],
     poaHealth: [
       {
-        profileKey: 'self', // Referencia a profiles.self
-        data: { /* Datos del POA Health para self */ },
-        documentDOM: { v1: {}, v2:{}},
+        profileKey: 'self', // Reference to self profile
+        data: { /* POA Health data for self */ },
+        documentDOM: { v1: {}, v2: {} },
       },
       {
-        profileKey: 'spouse', // Referencia a profiles.spouse
-        data: { /* Datos del POA Health para spouse */ },
-       documentDOM: { v1: {}, v2:{}},
+        profileKey: 'spouse', // Reference to spouse profile
+        data: { /* POA Health data for spouse */ },
+        documentDOM: { v1: {}, v2: {} },
       },
       {
-        profileKey: '1', // Referencia a profiles.others[1]
-        data: { /* Datos del POA Health para spouse */ },
-        documentDOM: { v1: {}, v2:{}},
+        profileKey: '1', // Reference to secondary profile 1
+        data: { /* POA Health data for secondary profile 1 */ },
+        documentDOM: { v1: {}, v2: {} },
       },
-      // Añadir más POA Healths según sea necesario
+      // Additional POA Health documents can be added as needed
     ],
-    // Otros documentos si es necesario
+    // Other documents can be added as necessary
   },
 };
