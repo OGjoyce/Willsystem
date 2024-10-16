5abf0b34725858311fc8a3f7a6044ed63264646f
abrahaamv <velizabrahaam@gmail.com>
2024-10-16 04:51:07 -0600

    Revert "Update DocumentSelector.jsx"
    This reverts commit 3d944dd314a10d167e6e2c43d4e1ff636f5fe5ec.

--- resources/js/Components/PDF/DocumentSelector.jsx
+++ resources/js/Components/PDF/DocumentSelector.jsx
@@ -94,7 +94,7 @@ const DocumentSelector = ({
         let documentFound = false;
 
         documentKeys.forEach((documentKey) => {
-            if (documentKey === doc && documents[documentKey].owner === 'unknown' && !documentFound && currentProfile !== null) {
+            if (documentKey === doc && documents[documentKey].owner === 'unknown' && !documentFound) {
                 // Asignar currentProfile como owner
                 const updatedObjectStatus = objectStatus.map((profileArray) => {
                     return profileArray.map((dataObj) => {
@@ -137,7 +137,7 @@ const DocumentSelector = ({
             }
         });
 
-        if (true) {
+        if (!documentFound) {
             // Si no se encuentra el documento con owner "unknown", mostrar el modal de confirmación
             const owner = documents[doc].owner;
 
