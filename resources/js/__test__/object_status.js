971791112c1bb551dfc58b434159fd936c1c5510
abrahaamv <velizabrahaam@gmail.com>
2024-10-17 20:48:06 -0600

    Update object_status.js

--- resources/js/__test__/object_status.js
+++ resources/js/__test__/object_status.js
@@ -1,323 +1,247 @@
-const objectStatus = [
-    [
+[
+  [
+    {
+      "personal": {
+        "step": 0,
+        "title": "Personal Information",
+        "city": "Poptun",
+        "province": "Petén",
+        "country": "Guatemala",
+        "telephone": "+1 (111) 111-1111",
+        "fullName": "Henry Veliz",
+        "email": "henry@email.com",
+        "timestamp": 1729219261389
+      },
+      "owner": "henry@email.com",
+      "packageInfo": {
+        "id": 9,
+        "name": "Package",
+        "price": "$1000",
+        "description": "Two spousal wills and four POAs",
+        "campaign": "organic",
+        "created_at": "2024-10-18T00:41:52.000000Z",
+        "updated_at": "2024-10-18T00:41:52.000000Z",
+        "documents": [
+          {
+            "id": 1,
+            "docType": "primaryWill",
+            "owner": "henry@email.com",
+            "dataStatus": "incomplete",
+            "willIdentifier": "primaryWill_1"
+          },
+          {
+            "id": 2,
+            "docType": "spousalWill",
+            "owner": "unknown",
+            "dataStatus": "incomplete",
+            "willIdentifier": "spousalWill_1"
+          },
+          {
+            "id": 3,
+            "docType": "poaProperty",
+            "owner": "unknown",
+            "dataStatus": "incomplete",
+            "associatedWill": "primaryWill_1"
+          },
+          {
+            "id": 4,
+            "docType": "poaProperty",
+            "owner": "unknown",
+            "dataStatus": "incomplete",
+            "associatedWill": "primaryWill_1"
+          },
+          {
+            "id": 5,
+            "docType": "poaHealth",
+            "owner": "unknown",
+            "dataStatus": "incomplete",
+            "associatedWill": "spousalWill_1"
+          },
+          {
+            "id": 6,
+            "docType": "poaHealth",
+            "owner": "unknown",
+            "dataStatus": "incomplete",
+            "associatedWill": "spousalWill_1"
+          }
+        ]
+      }
+    },
+    {
+      "marriedq": {
+        "selection": "true",
+        "timestamp": 1729219265702
+      }
+    },
+    {
+      "married": {
+        "firstName": "Jennifer",
+        "middleName": "",
+        "lastName": "Salguero",
+        "relative": "Spouse",
+        "email": "jennifer@email.com",
+        "phone": "+1 (111) 111-1111",
+        "city": "Poptun",
+        "province": "Petén",
+        "country": "Guatemala",
+        "timestamp": 1729219300064
+      }
+    },
+    {
+      "kidsq": {
+        "selection": "true",
+        "timestamp": 1729219301880
+      }
+    },
+    {
+      "kids": [
         {
-            "personal": {
-                "step": 0,
-                "title": "Personal Information",
-                "city": "Mahmud-e Raqi",
-                "province": "K\u0101p\u012bs\u0101",
-                "country": "Afghanistan",
-                "telephone": "+1 (111) 111-1111",
-                "fullName": "uno",
-                "email": "1@email.com",
-                "timestamp": 1729050144580
-            },
-            "owner": "1@email.com",
-            "packageInfo": {
-                "id": 5,
-                "name": "Package",
-                "price": "$1000",
-                "description": "One will and two POAs",
-                "campaign": "organic",
-                "created_at": "2024-10-16T00:38:00.000000Z",
-                "updated_at": "2024-10-16T00:38:00.000000Z",
-                "documents": {
-                    "primaryWill": {
-                        "id": 1,
-                        "owner": "1@email.com",
-                        "dataStatus": "incomplete"
-                    },
-                    "poaProperty": {
-                        "id": 2,
-                        "owner": "unknown",
-                        "dataStatus": "incomplete"
-                    },
-                    "poaHealth": {
-                        "id": 3,
-                        "owner": "unknown",
-                        "dataStatus": "incomplete"
-                    }
-                }
-            }
+          "id": 1,
+          "firstName": "Junior",
+          "lastName": "Véliz",
+          "relative": "Child",
+          "city": "Poptun",
+          "country": "Guatemala",
+          "province": "Petén"
         },
         {
-            "marriedq": {
-                "selection": "false",
-                "timestamp": 1729050145831
-            }
-        },
-        {
-            "married": {
-                "timestamp": 1729050145852
-            }
-        },
-        {
-            "kidsq": {
-                "selection": "false",
-                "timestamp": 1729050146473
-            }
-        },
-        {
-            "kids": []
-        },
-        {
-            "executors": [
-                {
-                    "firstName": "d",
-                    "middleName": "d",
-                    "lastName": "d",
-                    "relative": "Brother",
-                    "email": "e@gsdg.vo",
-                    "phone": "+1 (111) 111-1111",
-                    "city": "Mahmud-e Raqi",
-                    "province": "K\u0101p\u012bs\u0101",
-                    "country": "Afghanistan",
-                    "id": 2,
-                    "priority": 1
-                }
-            ],
-            "relatives": [
-                {
-                    "firstName": "d",
-                    "middleName": "d",
-                    "lastName": "d",
-                    "relative": "Brother",
-                    "email": "e@gsdg.vo",
-                    "phone": "+1 (111) 111-1111",
-                    "city": "Mahmud-e Raqi",
-                    "province": "K\u0101p\u012bs\u0101",
-                    "country": "Afghanistan",
-                    "id": 1
-                }
-            ]
-        },
-        {
-            "relatives": []
-        },
-        {
-            "bequests": []
-        },
-        {
-            "residue": []
-        },
-        {
-            "wipeout": []
-        },
-        {
-            "trusting": []
-        },
-        {
-            "guardians": []
-        },
-        {
-            "pets": []
-        },
-        {
-            "additional": []
-        },
-        {
-            "poaProperty": []
-        },
-        {
-            "poaHealth": []
-        },
-        {
-            "finalDetails": []
-        },
-        {
-            "documentDOM": []
+          "id": 2,
+          "firstName": "Helen",
+          "lastName": "Veliz",
+          "relative": "Child",
+          "city": "Poptun",
+          "country": "Guatemala",
+          "province": "Petén"
         }
-    ],
-    [
+      ]
+    },
+    {
+      "executors": [
         {
-            "personal": {
-                "step": 0,
-                "title": "Personal Information",
-                "city": "Kandahar",
-                "province": "Kandah\u0101r",
-                "country": "Afghanistan",
-                "telephone": "+1 (111) 111-1111",
-                "fullName": "dos",
-                "email": "2@email.com",
-                "timestamp": 1729050184234
-            },
-            "owner": "2@email.com",
-            "packageInfo": {
-                "id": 5,
-                "name": "Package",
-                "price": "$1000",
-                "description": "One will and two POAs",
-                "campaign": "organic",
-                "created_at": "2024-10-16T00:38:00.000000Z",
-                "updated_at": "2024-10-16T00:38:00.000000Z",
-                "documents": {
-                    "primaryWill": {
-                        "id": 1,
-                        "owner": "2@email.com",
-                        "dataStatus": "incomplete"
-                    },
-                    "poaProperty": {
-                        "id": 2,
-                        "owner": "unknown",
-                        "dataStatus": "incomplete"
-                    },
-                    "poaHealth": {
-                        "id": 3,
-                        "owner": "unknown",
-                        "dataStatus": "incomplete"
-                    }
-                }
-            }
+          "firstName": "Jennifer",
+          "middleName": "",
+          "lastName": "Salguero",
+          "relative": "Spouse",
+          "email": "jennifer@email.com",
+          "phone": "+1 (111) 111-1111",
+          "city": "Poptun",
+          "province": "Petén",
+          "country": "Guatemala",
+          "timestamp": 1729219300064,
+          "priority": 1,
+          "id": 1
         },
         {
-            "marriedq": {
-                "selection": "false",
-                "timestamp": 1729050185274
-            }
-        },
-        {
-            "married": {
-                "timestamp": 1729050185281
-            }
-        },
-        {
-            "kidsq": {
-                "selection": "false",
-                "timestamp": 1729050191708
-            }
-        },
-        {
-            "kids": []
-        },
-        {
-            "executors": [
-                {
-                    "firstName": "d",
-                    "middleName": "d",
-                    "lastName": "d",
-                    "relative": "Brother",
-                    "email": "e@gsdg.vo",
-                    "phone": "+1 (111) 111-1111",
-                    "city": "Mahmud-e Raqi",
-                    "province": "K\u0101p\u012bs\u0101",
-                    "country": "Afghanistan",
-                    "id": 2,
-                    "priority": 1
-                }
-            ],
-            "relatives": [
-                {
-                    "firstName": "d",
-                    "middleName": "d",
-                    "lastName": "d",
-                    "relative": "Brother",
-                    "email": "e@gsdg.vo",
-                    "phone": "+1 (111) 111-1111",
-                    "city": "Mahmud-e Raqi",
-                    "province": "K\u0101p\u012bs\u0101",
-                    "country": "Afghanistan",
-                    "id": 1
-                },
-                {
-                    "firstName": "ds",
-                    "middleName": "sd",
-                    "lastName": "sd",
-                    "relative": "Brother",
-                    "email": "e@e.com",
-                    "phone": "+1 (111) 111-1111",
-                    "city": "Mahmud-e Raqi",
-                    "province": "K\u0101p\u012bs\u0101",
-                    "country": "Afghanistan"
-                }
-            ]
-        },
-        {
-            "relatives": []
-        },
-        {
-            "bequests": {
-                "0": {
-                    "id": 1,
-                    "names": "d d",
-                    "backup": "NA",
-                    "shares": "100",
-                    "bequest": "d",
-                    "isCustom": false,
-                    "shared_uuid": 0
-                },
-                "timestamp": 1729050262669
-            }
-        },
-        {
-            "residue": {
-                "selected": "Have the residue go to parents then siblings per stirpes",
-                "timestamp": 1729050266537
-            }
-        },
-        {
-            "wipeout": {
-                "wipeout": {
-                    "selectedCategory": null,
-                    "selectedOption": null,
-                    "custom": false,
-                    "table_dataBequest": [],
-                    "availableShares": 100
-                },
-                "timestamp": 1729050271712
-            }
-        },
-        {
-            "trusting": {
-                "0": {
-                    "id": 1,
-                    "age": "21",
-                    "shares": "100"
-                },
-                "timestamp": 1729050278035
-            }
-        },
-        {
-            "guardians": []
-        },
-        {
-            "pets": {
-                "0": {
-                    "id": 3,
-                    "petName": "pet1",
-                    "guardian": "d d",
-                    "backup": "ds sd",
-                    "amount": 100
-                },
-                "timestamp": 1729050289416
-            }
-        },
-        {
-            "additional": {
-                "customClauseText": "",
-                "otherWishes": "",
-                "checkboxes": {
-                    "organdonation": true,
-                    "cremation": false,
-                    "buried": true
-                },
-                "timestamp": 1729050292775
-            }
-        },
-        {
-            "poaProperty": []
-        },
-        {
-            "poaHealth": []
-        },
-        {
-            "finalDetails": {
-                "specialInstructions": "sdd",
-                "pickup": false,
-                "timestamp": 1729050294792
-            }
-        },
-        {
-            "documentDOM": []
+          "id": 2,
+          "firstName": "Junior",
+          "lastName": "Véliz",
+          "relative": "Child",
+          "city": "Poptun",
+          "country": "Guatemala",
+          "province": "Petén",
+          "uuid": 1,
+          "priority": 1
         }
-    ]
+      ],
+      "relatives": []
+    },
+    {
+      "relatives": []
+    },
+    {
+      "bequests": {
+        "0": {
+          "id": 1,
+          "names": "Spouse First",
+          "backup": "Junior Véliz",
+          "shares": 100,
+          "bequest": "Gold chain",
+          "isCustom": false,
+          "shared_uuid": 0
+        },
+        "timestamp": 1729219349621
+      }
+    },
+    {
+      "residue": {
+        "selected": "Have the residue go to children per capita",
+        "timestamp": 1729219352403
+      }
+    },
+    {
+      "wipeout": {
+        "wipeout": {
+          "selectedCategory": "50% to parents and siblings and 50% to parents and siblings of spouse",
+          "selectedOption": null,
+          "custom": false,
+          "table_dataBequest": [],
+          "availableShares": 100
+        },
+        "timestamp": 1729219355925
+      }
+    },
+    {
+      "trusting": {
+        "0": {
+          "id": 1,
+          "age": "21",
+          "shares": "100"
+        },
+        "timestamp": 1729219363611
+      }
+    },
+    {
+      "guardians": {
+        "0": {
+          "id": 1,
+          "guardian": "Jennifer Salguero",
+          "position": 1
+        },
+        "1": {
+          "id": 2,
+          "guardian": "Junior Véliz",
+          "position": 2
+        },
+        "timestamp": 1729219378287
+      }
+    },
+    {
+      "pets": {
+        "0": {
+          "id": 4,
+          "petName": "Terry",
+          "guardian": "Jennifer Salguero",
+          "backup": "Junior Véliz",
+          "amount": 1000
+        },
+        "timestamp": 1729219389635
+      }
+    },
+    {
+      "additional": {
+        "customClauseText": "",
+        "otherWishes": "",
+        "checkboxes": {
+          "organdonation": true,
+          "cremation": true,
+          "buried": false
+        },
+        "timestamp": 1729219395733
+      }
+    },
+    {
+      "poaProperty": []
+    },
+    {
+      "poaHealth": []
+    },
+    {
+      "finalDetails": {
+        "timestamp": 1729219397652
+      }
+    },
+    {
+      "documentDOM": []
+    }
+  ]
 ]
