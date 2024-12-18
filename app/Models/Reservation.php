9b87056f308d69c4e5ebdeb2be5e4e7cc9519274
abrahaamv <velizabrahaam@gmail.com>
2024-12-18 12:14:53 -0600

    Update Reservation.php

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,24 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-protected $fillable = [
-    'lawyer_id',
-    'lawyer_email',
-    'client_name',
-    'client_email',
-    'title',
-    'description',
-    'start_date',
-    'end_date',
-    'duration',
-    'link',
-    'object_status_id'
-];
-   
+    protected $fillable = [
+        'lawyer_id',
+        'lawyer_email',
+        'client_name',
+        'client_email',
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'duration',
+        'link',
+        'object_status_id'
+    ];
 
+    public function lawyer()
+    {
+        return $this->belongsTo(Lawyer::class);
+    }
 }
