0d8f0c8ca0ccf69e0f83ee64812568637b8db8fa
abrahaamv <velizabrahaam@gmail.com>
2024-12-18 12:14:48 -0600

    Update AvailabilitySlot.php

--- app/Models/AvailabilitySlot.php
+++ app/Models/AvailabilitySlot.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AvailabilitySlot extends Model
 {
     protected $fillable = ['lawyer_id', 'day_of_week', 'start_time', 'end_time'];
+
+    public function lawyer()
+    {
+        return $this->belongsTo(Lawyer::class);
+    }
 }
