131be6741df93312ee2394027643a469dccf9ae6
abrahaamv <velizabrahaam@gmail.com>
2024-12-18 15:51:58 -0600

    Create ReservationUI.jsx

---
+++ resources/js/Components/Scheduler/ReservationUI.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReservationScheduler from "./ReservationScheduler";
+
+export function SchedulerMatrix() {
+    return (
+        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+            <ReservationScheduler />
+        </div>
+    );
+}
