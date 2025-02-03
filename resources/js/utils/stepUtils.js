9f1ec14269a0348d52440216bc03f1e402eb969a
Abrahaam Véliz <velizabrahaam@gmail.com>
2025-02-03 13:00:48 -0600

    Update stepUtils.js

--- resources/js/utils/stepUtils.js
+++ resources/js/utils/stepUtils.js
@@ -40,9 +40,11 @@ const stepper = [
             if (index === 10 && !hasKids) return false; // Guardian For Minors
 
             // Lógica basada en currentDocument
-            if (currentDocument === 'primaryWill' || currentDocument === 'spousalWill' || currentDocument === 'secondaryWill') {
+            if (currentDocument === 'primaryWill' || currentDocument === 'spousalWill') {
                 // Ocultar pasos 12 (POA Property) y 13 (POA Health) si es un primaryWill
                 if (index === 12 || index === 13) return false;
+            } else if (currentDocument === 'secondaryWill') {
+                if ( index === 7 || index === 10 || index === 11 || index === 12 || index === 13 || index === 14) return false;
             } else if (currentDocument === 'poaProperty') {
                 // Ocultar todos menos el paso 12 para POA Property
                 if (index === 5 || index === 6 || index === 7 || index === 8 || index === 9 ||
