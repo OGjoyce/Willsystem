54a49348a09ef0d72ab55224e127c27d0ab8ba99
abrahaamv <velizabrahaam@gmail.com>
2025-02-13 16:28:43 -0600

    Update DocumentsApprovalController.php

--- app/Http/Controllers/DocumentsApprovalController.php
+++ app/Http/Controllers/DocumentsApprovalController.php
@@ -5,39 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
+use App\Models\ObjStatus;
 
 class DocumentsApprovalController extends Controller
 {
     public function show(Request $request)
     {
-        // check if user is authenticated
         if (!Auth::check()) {
-            // Save the url in the session and send the user to login
             $request->session()->put('returnUrl', $request->fullUrl());
             return Redirect::route('login');
         }
 
         $token = $request->query('token');
 
+        if (!$token) {
+            // Obtener todos los object statuses sin filtrar y filtrar en el frontend
+            $objectStatuses = ObjStatus::all();
+
+            return Inertia::render('Admin/ObjectStatusSelection', [
+                'objectStatuses' => $objectStatuses
+            ]);
+        }
+
         try {
-            // Decrypt token
             $payload = Crypt::decryptString($token);
             $data = json_decode($payload, true);
 
-            $email = $data['email'];
-            $id = $data['id'];
-         
-
-            // Check if email in token is the same as the authenticated email
-            if (Auth::user()->email !== $email) {
+            if (Auth::user()->email !== $data['email']) {
                 abort(403, 'Unauthorized');
             }
 
             return Inertia::render('Admin/DocumentsApproval', [
-                'id' => $id,
+                'id' => $data['id']
             ]);
         } catch (\Exception $e) {
             abort(403, 'Invalid token');
